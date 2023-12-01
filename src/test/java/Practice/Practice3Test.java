66d8ef3c56b949cdd9954e71fb05c2b99ecd4bdf
AAdhvikashruthi <shruthigm14@gmail.com>
2023-12-01 15:45:35 +0530

    1st commit

---
+++ src/test/java/Practice/Practice3Test.java
@@ -0,0 +1,16 @@
+package Practice;
+
+import org.testng.annotations.Test;
+
+public class Practice3Test {
+	@Test
+	public void createTest()
+	{
+		System.out.println("test1 is created");
+	}
+	@Test
+	public void createTest1()
+	{
+		System.out.println("test2 is created");
+	}
+}
