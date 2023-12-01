1258b412efe85fc12bd56646de58a6d55e6e532a
susendrants <susee.jt@gmail.com>
2023-12-01 15:37:25 +0530

    1c

---
+++ src/test/java/Practice/Practice1Test.java
@@ -0,0 +1,11 @@
+package Practice;
+import org.testng.annotations.Test;
+
+public class Practice1Test {
+	@Test
+	public void m1()
+	{
+		System.out.println("Hello Everyone kooi");
+	}
+
+}
---
+++ src/test/java/Practice/Practice2Test.java
@@ -0,0 +1,12 @@
+package Practice;
+
+import org.testng.annotations.Test;
+
+public class Practice2Test {
+	@Test
+	public void m2()
+	{
+		System.out.println("How ypu doing");
+	}
+
+}
